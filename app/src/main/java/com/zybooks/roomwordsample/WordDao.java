e3710442ae9005c4dcac3af79a953e9f5a46bc56
Shahin Nezam <shahinnezam@hotmail.com>
2022-01-26 19:59:48 -0800

    Added New Word activity, fixed some errors throughout

--- app/src/main/java/com/zybooks/roomwordsample/WordDao.java
+++ app/src/main/java/com/zybooks/roomwordsample/WordDao.java
@@ -13,14 +13,16 @@ public interface WordDao {
 
     // allowing the insert of the same word multiple times by passing a
     // conflict resolution strategy
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
     void deleteAll();
 
-    @Query("SELECT * FROM word_table ORDER BY word ASC")
-    LiveData<List<Word>> getAlphabetizedWords();
+
 }
 
 
--- app/src/main/java/com/zybooks/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/zybooks/roomwordsample/WordRoomDatabase.java
@@ -2,17 +2,19 @@ package com.zybooks.roomwordsample;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
-public abstract class WordRoomDatabase extends RoomDatabase {
+abstract class WordRoomDatabase extends RoomDatabase {
 
-    public abstract WordDao wordDao();
+    abstract WordDao wordDao();
 
     private static volatile WordRoomDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
@@ -25,10 +27,33 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
 }
+
